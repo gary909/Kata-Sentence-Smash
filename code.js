52ede446e0de6fbc6094739e05b371578c4e9347
gary <gary.white@lodgit.com>
2021-11-16 13:23:21 +0100

    code working

---
+++ code.js
@@ -0,0 +1,7 @@
+// Smash Words
+function smash (words) {
+    return words.join(" ");  
+};
+
+console.log(smash(["hello"])); // returns 'hello'
+console.log(smash(["hello", "world"])); // returns 'hello world'
